1c206b7ea9c223a0c9a8078131dd0ab37e0c2be6
ciaodiu <32728900+ciaodiu@users.noreply.github.com>
2023-01-16 16:17:31 +0800

    Create Verifysig.sol

---
+++ Demo/Verifysig.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+
+// 签名验证合约, 签名\校验\恢复
+contract Verifysig {
+    address immutable public signer;
+
+    constructor() {
+        signer = 0x4C7DaB8b982aa2580F7D59f0bcac103b949b1cd7;
+    }
+
+    /**
+     * @dev 通过ECDSA，验证签名地址是否正确，如果正确则返回true
+     * price为原始价格数据
+     * _signature为签名
+     */
+    function main(string memory price, bytes memory _signature) public view returns (bool) {
+        bytes32 _msgHash = getMessageHash(price); // 将price打包成hash
+        bytes32 _ethSignedMessageHash = toEthSignedMessageHash(_msgHash); // 计算以太坊签名消息
+        return verify(_ethSignedMessageHash, _signature, signer); // ECDSA检验通过
+    }
+
+    function getMessageHash(string memory price) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(price));
+    }
+
+    /**
+     * @dev 通过ECDSA，验证签名地址是否正确，如果正确则返回true
+     * _msgHash为消息的hash
+     * _signature为签名
+     * _signer为签名地址
+     */
+    function verify(bytes32 _msgHash, bytes memory _signature, address _signer) public pure returns (bool) {
+        return recoverSigner(_msgHash, _signature) == _signer;
+    }
+
+    // @dev 从_msgHash和签名_signature中恢复signer地址
+    function recoverSigner(bytes32 _msgHash, bytes memory _signature) public pure returns (address){
+        // 检查签名长度，65是标准r,s,v签名的长度
+        require(_signature.length == 65, "invalid signature length");
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+        // 目前只能用assembly (内联汇编)来从签名中获得r,s,v的值
+        assembly {
+            /*
+            前32 bytes存储签名的长度 (动态数组存储规则)
+            add(sig, 32) = sig的指针 + 32
+            等效为略过signature的前32 bytes
+            mload(p) 载入从内存地址p起始的接下来32 bytes数据
+            */
+            // 读取长度数据后的32 bytes
+            r := mload(add(_signature, 0x20))
+            // 读取之后的32 bytes
+            s := mload(add(_signature, 0x40))
+            // 读取最后一个byte
+            v := byte(0, mload(add(_signature, 0x60)))
+        }
+        // 使用ecrecover(全局函数)：利用 msgHash 和 r,s,v 恢复 signer 地址
+        return ecrecover(_msgHash, v, r, s);
+    }
+    
+    /**
+     * @dev 返回 以太坊签名消息
+     * `hash`：消息哈希 
+     * 遵从以太坊签名标准：https://eth.wiki/json-rpc/API#eth_sign[`eth_sign`]
+     * 以及`EIP191`:https://eips.ethereum.org/EIPS/eip-191`
+     * 添加"\x19Ethereum Signed Message:\n32"字段，防止签名的是可执行交易。
+     */
+    function toEthSignedMessageHash(bytes32 hash) public pure returns (bytes32) {
+        // 32 is the length in bytes of hash,
+        // enforced by the type signature above
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+}
